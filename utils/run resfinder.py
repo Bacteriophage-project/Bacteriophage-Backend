3f40c20c71273328b320f9a33dc5c6ce978bf0a8
CynthiaVaati <146328564+CynthiaVaati@users.noreply.github.com>
2025-09-03 12:15:09 +0300

    Update run resfinder.py

--- utils/run resfinder.py
+++ utils/run resfinder.py
@@ -1,1 +1,6 @@
-
+from pathlib import Path
+from utils.download_genomes import download_and_decompress_fasta
+import re
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
